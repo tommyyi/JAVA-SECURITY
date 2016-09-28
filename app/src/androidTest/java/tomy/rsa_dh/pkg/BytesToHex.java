36edc527e0e6241914c9fe55dd90829db34bc4a4
3033593256@qq.com <3033593256@qq.com>
2016-09-28 10:33:43 +0800

    a little change

--- app/src/androidTest/java/tomy/rsa_dh/pkg/BytesToHex.java
+++ app/src/androidTest/java/tomy/rsa_dh/pkg/BytesToHex.java
@@ -1,18 +1,23 @@
 package tomy.rsa_dh.pkg;
 
-public class BytesToHex {
-	
-	public static String fromBytesToHex(byte[] resultBytes) {
-		StringBuilder builder = new StringBuilder();
-		for (int i = 0; i < resultBytes.length; i++) {
-			if (Integer.toHexString(0xFF & resultBytes[i]).length() == 1) {
-				builder.append("0").append(
-						Integer.toHexString(0xFF & resultBytes[i]));
-			} else {
-				builder.append(Integer.toHexString(0xFF & resultBytes[i]));
-			}
-		}
-		return builder.toString();
-	}
+public class BytesToHex
+{
+
+    public static String fromBytesToHex(byte[] resultBytes)
+    {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < resultBytes.length; i++)
+        {
+            if (Integer.toHexString(0xFF & resultBytes[i]).length() == 1)
+            {
+                builder.append("0").append(Integer.toHexString(0xFF & resultBytes[i]));
+            }
+            else
+            {
+                builder.append(Integer.toHexString(0xFF & resultBytes[i]));
+            }
+        }
+        return builder.toString();
+    }
 
 }
